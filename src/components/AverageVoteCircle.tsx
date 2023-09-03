35d688626d37477eea19c4e6c542c5eef398df6d
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-09-03 02:23:13 +0100

    Refactor AverageVoteCircle component - handle case when averageVote is null

--- src/components/AverageVoteCircle.tsx
+++ src/components/AverageVoteCircle.tsx
@@ -6,6 +6,15 @@ interface Props {
 }
 
 const AverageVoteCircle = ({ averageVote }: Props) => {
+  if (!averageVote) {
+    return (
+      <CircularProgress value={0} color="gray.400" >
+        <CircularProgressLabel> 
+          <Text fontSize="sm" fontWeight="bold">NR</Text>
+        </CircularProgressLabel>
+      </CircularProgress>
+    )
+  }
   const color = averageVote > 7.5 ? "green.400" : averageVote > 6 ? "yellow.400" : "red.400"
   const votePercentage = parseInt((averageVote * 10).toFixed(0))
   return (
@@ -18,3 +27,4 @@ const AverageVoteCircle = ({ averageVote }: Props) => {
 }
 
 export default AverageVoteCircle
+
